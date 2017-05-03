144044e55d1e9080ae2b7bf31a99829a78110e24
techniko27 <nikoscheuermann@web.de>
2017-05-03 12:43:24 +0200

    extracted and moved renterpoints

--- src/Customer.java
+++ src/Customer.java
@@ -7,17 +7,20 @@ class Customer {
     private Vector rentals = new Vector();
     public Customer (String newname){
         name = newname;
-    };
+    }
+
     public void addRental(Rental arg) {
         rentals.addElement(arg);
-    };
+    }
+
     public String getName (){
         return name;
-    };
+    }
+
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
+        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
@@ -26,10 +29,7 @@ class Customer {
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
             // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints ++;
+            frequentRenterPoints += each.getFrequentRenterPoints(frequentRenterPoints);
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
             totalAmount += each.getCharge();
--- src/Rental.java
+++ src/Rental.java
@@ -33,4 +33,14 @@ class Rental {
         }
         return result;
         }
+
+    int getFrequentRenterPoints(int frequentRenterPoints)
+    {
+        frequentRenterPoints ++;
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
